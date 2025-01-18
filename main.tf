c7eef37dc14bf64fc7567e22e91901676abfc32f
Krzysztof Zwolenik <kzwolenik95@gmail.com>
2025-01-18 18:10:50 +0100

    Change ec2 user

--- main.tf
+++ main.tf
@@ -18,16 +18,42 @@ resource "aws_instance" "ubuntu_instance" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
 
   user_data = <<-EOF
     #!/bin/bash
-    mkdir -p /home/ec2-user/.ssh
-    echo "${join("\n", var.ssh_public_keys)}" > /home/ec2-user/.ssh/authorized_keys
-    chown -R ec2-user:ec2-user /home/ec2-user/.ssh
-    chmod 600 /home/ec2-user/.ssh/authorized_keys
+    mkdir -p /home/ubuntu/.ssh
+    echo "${join("\n", var.ssh_public_keys)}" > /home/ubuntu/.ssh/authorized_keys
+    chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+    chmod 600 /home/ubuntu/.ssh/authorized_keys
   EOF
 
   tags = {
     Name = "ssh-remote-server-setup"
   }
 }
+
+data "http" "my_ip" {
+  url = "http://checkip.amazonaws.com"
+}
+
+resource "aws_security_group" "ssh_access" {
+  name_prefix = "ssh-access-"
+
+  ingress {
+    description      = "Allow SSH from my IP"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["${chomp(data.http.my_ip.response_body)}/32"] # Replace with your public IP
+  }
+
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
